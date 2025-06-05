569ed07fd2d1df543b36fc61d6f20e1a2fe33dc1
Rim-2 <turtle2260@gmail.com>
2025-06-05 16:22:17 +0900

    unicode

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
-    public static void printStudent(String name, int age) {
-        System.out.println("이름: " + name);
-        System.out.println("나이: " + age);
-        System.out.println("----------------");
-    }
     public static void main(String[] args) {
-        System.out.println("===학생 정보 출력===");
-        printStudent("Alice", 20);
-        printStudent("Bob", 22);
-        printStudent("Charlie", 25);
+        char asciiChar = 'A';
+        System.out.println("Character: " + asciiChar);
+
+        int asciiCode= (int) asciiChar;
+        System.out.println("ASCII Code: " + asciiCode);
+
+        char koreanChar = '가';
+        System.out.println("Character: "+ koreanChar);
+
+        int uniCode2 = (int) koreanChar;
+        System.out.println("ASCII Code: U+" + Integer.toHexString(uniCode2).toUpperCase());
     }
 }
