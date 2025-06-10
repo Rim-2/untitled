aafe67709be5217844dcdffc674dbc6544d79df3
Rim-2 <turtle2260@gmail.com>
2025-06-10 11:10:37 +0900

    class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+class Person {
+    String name;
+    int age;
+    boolean hasJob;
+    String job;
+
+    public void sayHello() {
+        System.out.println("안녕하세요." + name + "입니다. 저의 나이는" + age + "세 입니다.");
+    }
+
+    public void sayJob() {
+        if (hasJob) {
+            System.out.println("저는 무직입니다.");
+        } else {
+            System.out.println("제 직업은 " + job + "입니다.");
+        }
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
+        Person person = new Person();
+        person.name = "Alice";
+        person.hasJob = true;
+        person.job = "개발자";
 
-        System.out.println(address.substring(0,7));
-
-        System.out.println(address.substring(address.length()-5));
+        person.sayHello();
+        person.sayJob();
     }
 }
