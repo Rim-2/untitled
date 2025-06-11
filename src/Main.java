fca6d2c6bdbd78b0e6b7e653371862713e86e855
Rim-2 <turtle2260@gmail.com>
2025-06-11 13:12:47 +0900

    printf

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
-class Car {
-    String brand;
-    int year;
-
-    public Car() {
-    }
-
-    public Car(String brand, int year) {
-        this.brand = brand;
-        this.year = year;
-    }
-
-    public void displayInfo() {
-        System.out.println("Car brand: " + brand + ", year: " + year);
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("현대", 2024);
-        car.displayInfo();
+        int month = 6;
+        int temperature = 27;
+        double pi = 3.14159;
+
+        System.out.printf("현재는 %d월, 기온: %d, 원주율: %.2f", month, temperature, pi);
     }
 }
