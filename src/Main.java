d185467d2ef37df64f187614ab53d87ce3030cf3
Rim-2 <turtle2260@gmail.com>
2025-06-11 10:29:49 +0900

    constructor

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,24 @@
-
 class Car {
     String brand;
     int year;
-    int number;
-    String color;
 
     public Car() {
     }
 
-    public Car(String brand, int year, int number, String color) {
+    public Car(String brand, int year) {
         this.brand = brand;
         this.year = year;
-        this.number = number;
-        this.color = color;
     }
 
     public void displayInfo() {
-        System.out.println("Car brand: " + brand + ", year: " + year + ", number: " + number + ", color: " + color);
+        System.out.println("Car brand: " + brand + ", year: " + year);
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("현대", 2024, 9212, "검정색");
+        Car car = new Car("현대", 2024);
         car.displayInfo();
     }
 }
