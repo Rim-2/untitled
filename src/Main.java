638db4a8c6548fbd9edf6bffddab98545a201a22
Rim-2 <turtle2260@gmail.com>
2025-06-13 14:31:55 +0900

    interface

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,18 @@
-abstract class Shape {
-    abstract double area();
+interface Animal {
+    void makeSound();
 }
 
-class Circle extends Shape {
-    double radious;
-
-    public Circle(double radious) {
-        this.radious = radious;
-    }
-
+class Dog implements Animal {
     @Override
-    public double area() {
-        return radious * radious * Math.PI;
-    }
-}
-
-class Rectangle extends Shape {
-    double height;
-    double width;
-
-    public Rectangle (double height, double length) {
-        this.height = height;
-        this.width = length;
-    }
-
-    @Override
-    public double area() {
-        return height * width;
+    public void makeSound() {
+        System.out.println("멍멍!");
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
-
-        shapes[0] = new Circle(3);
-        shapes[1] = new Rectangle(4, 5);
-
-        for (Shape shape:shapes) {
-            System.out.println(shape.area());
-        }
+        Animal dog = new Dog();
+        dog.makeSound();
     }
 }
