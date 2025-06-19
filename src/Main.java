290c7d34e2a793000a54a8a3f5b012814390d8b5
Rim-2 <turtle2260@gmail.com>
2025-06-19 13:14:02 +0900

    generic with arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Box {
-    public static <T> void displayArray(T[] array) {
-        for (T element : array) {
+    public static <T> void displayArray(List<T> array) {
+        for(T element: array) {
             System.out.println(element);
         }
     }
@@ -8,8 +11,16 @@ class Box {
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] numbers = {1, 2, 3, 4, 5};
-        String[] words = {"apple", "banana", "cherry"};
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("cherry");
 
         Box.displayArray(numbers);
         Box.displayArray(words);
