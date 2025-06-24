22265869cf4af47582219a6d54a9b79ae311da02
Rim-2 <turtle2260@gmail.com>
2025-06-24 11:36:20 +0900

    file writer

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,17 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Method;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
-@interface CustomInfo {
-    String author();
-    String date();
-    int version() default 1;
-}
-
-@CustomInfo(author = "Rim2", date = "2025-06-24", version = 2)
-class Demo {
-
-    @CustomInfo(author = "Rim2", date = "2025-06-24")
-    public void display() {
-        System.out.println("Display method executed.");
-    }
-
-}
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+    public static void writeFile(String filename, String content) {
+        try (FileWriter writer = new FileWriter(filename, true)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        Demo demo = new Demo();
-        Class<?> demoClass = demo.getClass();
-
-        if (demoClass.isAnnotationPresent(CustomInfo.class)) {
-            CustomInfo info = demoClass.getAnnotation(CustomInfo.class);
-            System.out.println("Author: " + info.author());
-            System.out.println("Date: " + info.date());
-            System.out.println("Version: " + info.version());
-        }
-
-        try {
-            Method m = demoClass.getMethod("display");
-            if(m.isAnnotationPresent(CustomInfo.class)) {
-                CustomInfo mi = m.getAnnotation(CustomInfo.class);
-                System.out.println("Author: " + mi.author());
-                System.out.println("Date: " + mi.date());
-                System.out.println("Version: " + mi.version());
-            }
-        } catch (NoSuchMethodException e) {
-            e.getStackTrace();
-        }
+        writeFile("example.txt", "쭈꾸미 먹어야겠다");
+        System.out.println("파일 생성 완료");
     }
 }
